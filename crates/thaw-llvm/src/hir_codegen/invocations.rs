13dc11576a6ae3447a35463890f19fb155c8fb3b
NY1729 <nkgwyuu@gmail.com>
2026-09-09 00:53:43 +0900

    split JSON call codegen

--- crates/thaw-llvm/src/hir_codegen/invocations.rs
+++ crates/thaw-llvm/src/hir_codegen/invocations.rs
@@ -2,4 +2,5 @@ include!("invocations/calls.rs");
 include!("invocations/closure_calls.rs");
 include!("invocations/dynamic_calls.rs");
 include!("invocations/expression_calls.rs");
+include!("invocations/json_calls.rs");
 include!("invocations/variable_calls.rs");
